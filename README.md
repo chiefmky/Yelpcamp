8552089e809db22abfbfee1f56988ce71ac67474
Moise Mokoy <mosesmokoy@gmail.com>
2019-01-06 23:42:10 +0000

    mongolab

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # vFinal16
+# create project to heroku
+```
+git init
+git status
+git add .
+git commit -m ""
+heroku login -i
+heroku create
+git push heroku master
+```
+- add package.json
+```
+"start":"node app.js"
+```
--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes= require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_final_v16", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost:27017/yelp_camp_final_v16", { useNewUrlParser: true });
+mongoose.connect("mongodb://moise:brigitte0903@ds153705.mlab.com:53705/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
